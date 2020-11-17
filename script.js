0fe3eb7cf2afe391f2c0e77d1b9bf5c7d1b2e2bd
isaacionescu <isaac.ionescu@gmail.com>
2020-11-17 07:41:04 +0000

    v 2.0 - Critical improvements
    // HTML:
    + Added a hyperlink to Dogiac logo for easy navigation.
    + Removed outdated Bootstrap-type classes on the 12 buttons.
    
    // CSS:
    + Centered pretty much all content and text on page.
    - Replaced "bootstrap" type sectioning with flexbox and gridbox, where needed.
    + Media queries implemented for a mobile first approach: default is mobile (1 column), then tablet (3 cols), then desktop (4 cols).
    + Improved the look, spacing of buttons, sections, etc., overall.
    + Restructured content: .container is now separate from the .table below, and it comes after the header.
    + Changed animation technicalities. Isolated { display:none } as a separate class.
    + Fiddled with background cover properties for better adjustment. Still an issue, jumpy, must solve with some fixed height - but better than before.
    + Moved everything to the file style2.css where I implemented flexbox & gridbox (for safety).
    
    // Javascript:
    + Added filler text in the .zodiac module for all the 12 zodiacs / dogs, for proof of work. Actual text TBA.
    + In the App() class of script.js, I changed the behavior of instantiating the final result: now, the container and table are slowly dissapearing, then the whole content of container is deleted, and then replaced with the content of result.

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ class App {
 		this.zodiac = zodiac;
 		this.texte = texte;
 
-		this.$body = document.querySelectorAll('body');
+		this.$body = document.querySelector('body');
 		this.$button = document.querySelector('.button');
 		this.$container = document.querySelector('.container');
+		this.$table =document.querySelector('.table');
 
 		this.addEventListeners();
 	}
@@ -55,18 +56,36 @@ class App {
 		if(event.target.matches('.button')) {
 			const whichButton = event.target.closest('.button');
 			this.$container.classList.add('makeinvisible');
+			this.$table.classList.add('makeinvisible');
+			// this.$container.classList.add('display-none');
+			// this.$table.classList.add('display-none');
 			return whichButton
 		} 
 	}
 
 	populatePage(id) {
-		setTimeout(() => { this.$container.outerHTML = 
-			`
-			You are a ${this.zodiac[id].breed}! Woof woof 🐶
+		setTimeout( () => { 
+			const result = document.createElement('div');
+			result.classList.add('container');
 
-			As a ${this.zodiac[id].sign} born in the interval of ${this.zodiac[id].dates}
+			result.innerHTML = 
 			`
-		 }, 900);
+			You are a ${this.zodiac[id].breed}! Woof woof 🐶 <br><br>
+			You are a ${this.zodiac[id].sign} born in the interval of ${this.zodiac[id].dates}.
+			<br><br>
+			${this.texte[id]}
+			`
+
+			this.$container.innerHTML = "";
+			this.$container.appendChild(result);
+
+			this.$container.classList.remove('makeinvisible');
+
+
+			// this.$container.textContent = result;
+
+			console.log(result)
+		 }, 400);
 
 	// console.log(`Raspuns: ${this.zodiac[signID].breed}`)
 	}
--- zodiac.js
+++ zodiac.js
@@ -1,29 +1,68 @@
 export let zodiac = {
-	1: { sign: 'Aries', breed: 'Husky', dates: 'March 21 - April 19' },
-	2: { sign: 'Taurus', breed: 'Beagle', dates: 'April 20 - May 20' },
-	3: { sign: 'Gemini', breed: 'German Shepherd', dates: 'May 21-Jun 20' },
-	4: { sign: 'Cancer', breed: 'Dalmatian???', dates: 'Jun 21 - Jul 22' },
+	 1: { sign: 'Aries', breed: 'Husky', dates: 'March 21 - April 19' },
+	 2: { sign: 'Taurus', breed: 'Beagle', dates: 'April 20 - May 20' },
+	 3: { sign: 'Gemini', breed: 'German Shepherd', dates: 'May 21-Jun 20' },
+	 4: { sign: 'Cancer', breed: 'Dalmatian?', dates: 'Jun 21 - Jul 22' },
+	 5: { sign: 'Leo', breed: 'Leo breed', dates: 'Jul 23 - Aug 22' },
+	 6: { sign: 'Virgo', breed: 'Virgo breed', dates: 'Aug 23 - Sep 22' },
+	 7: { sign: 'Libra', breed: 'Libra breed', dates: 'Sep 23 - Oct 22' },
+	 8: { sign: 'Scorpio', breed: 'Scorpio breed', dates: 'Oct 23 - Nov 21' },
+	 9: { sign: 'Sagittarius', breed: 'Sagittarius breed', dates: 'Nov 22 - Dec 21' },
+	10: { sign: 'Capricorn', breed: 'Capricorn breed', dates: 'Dec 22 - Jan 19' },
+	11: { sign: 'Aquarius', breed: 'Aquarius breed', dates: 'Jan 20 - Feb 18' },
+	12: { sign: 'Pisces', breed: 'Pisces breed', dates: 'Feb 19 - March 20' },
 }
 
 
 export let texte = {
 	1: `
-		Husky / Aries (March 21 - April 19)
-		 is passionate, independent, free spirited, competitive, loyal, magnetic.
-		Huskys playful, free spirited, independent, need a lot of exercise. They howling like crazy.
+		Huskys are playful, free spirited, independent, need a lot of exercise, just like a typical Aries! They howl like crazy.
 	`,
 
 	2: `
-		Beagle / Taurus (April 20 - May 20)
-	Smart, ambitious, and trustworthy, Taurus is the anchor of the Zodiac. Amazing friends, colleagues, and partners, Taureans value honesty above all else and are proud that their personal relationships tend to be drama free.
+	With amazing friends, colleagues, and partners, Taureans are ambitious, trustworthy, value honesty above all else and are proud that their personal relationships tend to be drama free.
 	Beagles are active companions for kids and adults alike. But they can also be stubborn and require patience.
 	`,
 
 	3: `
-	Smart, passionate, and dynamic, Gemini is characterized by the Twins, Castor and Pollux, and is known for having two different sides they can display to the world. Expert communicators, Gemini is the chameleon of the Zodiac, adept at blending into different groups based on the vibe and energy they perceive. While they're also amazing at showcasing surface traits, the Gemini well runs deep, which is why the Twins are one of the Zodiac's most emotionally intelligent signs.
+	Smart, passionate, and dynamic, a Gemini is characterized by the Twins, Castor and Pollux, and is known for having two different sides they can display to the world. Expert communicators, Gemini is the chameleon of the Zodiac, adept at blending into different groups based on the vibe and energy they perceive. While they're also amazing at showcasing surface traits, the Gemini well runs deep, which is why the Twins are one of the Zodiac's most emotionally intelligent signs.
 	`,
 
-	4: ``
+	4: `
+	Sorry, I only have some filler text for Cancer at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	5: `
+	Sorry, I only have some filler text for Leo at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	6: `
+	Sorry, I only have some filler text for Virgo at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	7: `
+	Sorry, I only have some filler text for Libra at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	8: `
+	Sorry, I only have some filler text for Scorpio at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	9: `
+	Sorry, I only have some filler text for Sagittarius at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	10: `
+	Sorry, I only have some filler text for Capricorn at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	11: `
+	Sorry, I only have some filler text for Aquarius at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`,
+
+	12: `
+	Sorry, I only have some filler text for Pisces at the moment. When I'll find inspiration to connect it to a dog, I'll write a short paragraph here. In the meantime, go back to the home page and try another of the first few dogs! :)
+	`
 }
 
 
